038b074ee2310c88611fbbb57f44dc44c76efa1e
Rippanda12 <minecraftbgame@abv.bg>
2022-03-03 13:00:06 +0200

    repository

--- archiso/airootfs/usr/local/share/bredlocal/update_repo.sh
+++ archiso/airootfs/usr/local/share/bredlocal/update_repo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm local_repo*
+rm bredlocal*
 
 echo "repo-add"
-repo-add -n -R local_repo.db.tar.gz *.pkg.tar.zst
+repo-add -n -R bredlocal.db.tar.gz *.pkg.tar.zst
 
 echo "####################################"
 echo "Repo Updated!!"

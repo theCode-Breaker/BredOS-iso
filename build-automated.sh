66fd96fd0d41441b5e7fd13a6c6ea07891aae5ec
Adarsh <83777943+theCode-Breaker@users.noreply.github.com>
2022-03-11 20:07:47 +0530

    Update build-automated.sh

--- build-automated.sh
+++ build-automated.sh
@@ -1,3 +1,7 @@
-pacman -S --noconfirm --needed archlinux-keyring
+pacman -S --noconfirm --needed archlinux-keyring wget
+# Chaotic AUR
+echo "Fetching chaotic keys"
+pacman-key --init && pacman-key --recv-key 0706B90D37D9B881 FBA220DFC880C036 --keyserver keyserver.ubuntu.com && pacman-key --lsign-key 0706B90D37D9B881 FBA220DFC880C036 && pacman --noconfirm -U 'https://geo-mirror.chaotic.cx/chaotic-aur/chaotic-'{keyring,mirrorlist}'.pkg.tar.zst'
+
 mkarchiso -v -w ./work -o ./out ./archiso/
 echo "ISO Built in ./out folder!"
